c9c9842cf3c80b2ab6c91d6bdfe98df46c8a47b9
Nigel John <mytrini@att.net>
2016-08-04 13:59:25 -0400

    Updated Server.js

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ var cheerio 	= require('cheerio');
 
 //Database configuration
 var mongojs 	= require('mongojs');
+//var databaseUrl = "scraper";
 var databaseUrl = 'mongodb://heroku_6jtkwl62:hbcdvckrtl82oplaq0l584rcdn@ds023902.mlab.com:23902/heroku_6jtkwl62';
 var collections = ["scrapedData"];
 
 //Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
+var db = mongojs(databaseUrl);
 db.on('error', function(err){
   console.log('Database Error:', err);
 });
